678dd21dbabd13c8963c834069468cba6423d960
wenyongqd <wenyongqd@gmail.com>
2020-03-21 11:47:24 -0400

    update

---
+++ src/main/java/io/igileintelliengence/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package io.igileintelliengence.ppmtool.repository;
+
+import io.igileintelliengence.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
