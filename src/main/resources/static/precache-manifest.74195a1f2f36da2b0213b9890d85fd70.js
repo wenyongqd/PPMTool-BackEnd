116f29838e7b9863045e66a09d2bef37bbe050cf
wenyongqd <wenyongqd@gmail.com>
2020-07-13 15:24:55 -0400

    update static files

---
+++ src/main/resources/static/precache-manifest.74195a1f2f36da2b0213b9890d85fd70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ad24326a147c45528c7a3ba694fe68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629d4d502c3222a838e3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6c5799d90e4653d4150f",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "629d4d502c3222a838e3",
+    "url": "/static/js/2.df094538.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.df094538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5799d90e4653d4150f",
+    "url": "/static/js/main.3d3c7cde.chunk.js"
+  },
+  {
+    "revision": "09c60792fd0167d2a4ea",
+    "url": "/static/js/runtime-main.8688cc57.js"
+  }
+]);
